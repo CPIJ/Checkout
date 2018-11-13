86203e4f6106d8bc81158a39a28f033f6dee13e0
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-11-13 13:35:08 +0100

    Styled home screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <template>
     <div id="home-view">
-        <router-link tag="button" to="/shop">Winkel</router-link>
-        <router-link tag="button" to="/checkout">Afrekenen</router-link>
-        <router-link tag="button" to="/">Addons</router-link>
-        <hr>
-        <router-link v-if="admin" tag="button" to="/cash-register">Kassa</router-link>
-        <router-link v-if="admin" tag="button" to="/trained-images">Train afbeeldingen</router-link>
+      <div id="buttons">
+          <router-link tag="button" to="/shop">Winkel</router-link>
+          <router-link tag="button" to="/checkout">Afrekenen</router-link>
+          <router-link tag="button" to="/">Addons</router-link>
+          <router-link v-if="admin" tag="button" to="/cash-register">Kassa</router-link>
+          <router-link v-if="admin" tag="button" to="/trained-images">Afbeeldingen</router-link>
+        </div>
     </div>
 </template>
 
@@ -35,6 +36,27 @@ export default {
   display: flex;
   align-items: center;
   flex-direction: column;
-  vertical-align: middle;
+}
+
+#home-view button {
+  width: 50vw;
+  font-size: 1em;
+  margin: 0.3em;
+  padding: 0.5em;
+}
+
+#buttons {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+button {
+  border: none;
+  margin: 0;
+  padding: 0;
+  width: auto;
+  overflow: visible;
 }
 </style>
