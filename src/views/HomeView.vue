59f684fc153d1ad6cadf810ac892d024c9584ef1
Pijnenburg,Casper C.A.P <357629@student.fontys.nl>
2018-11-13 13:21:32 +0100

    Added links to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,40 @@
 <template>
-    <div id="homve-view">
-        Home
+    <div id="home-view">
+        <router-link tag="button" to="/shop">Winkel</router-link>
+        <router-link tag="button" to="/checkout">Afrekenen</router-link>
+        <router-link tag="button" to="/">Addons</router-link>
+        <hr>
+        <router-link v-if="admin" tag="button" to="/cash-register">Kassa</router-link>
+        <router-link v-if="admin" tag="button" to="/trained-images">Train afbeeldingen</router-link>
     </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      key: "",
+      admin: false
+    };
+  },
 
-}
+  mounted() {
+    window.addEventListener("keydown", e => (this.key += e.key));
+  },
+
+  watch: {
+    key(newVal) {
+      this.admin = newVal === "admin";
+    }
+  }
+};
 </script>
 
 <style>
-
+#home-view {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  vertical-align: middle;
+}
 </style>
