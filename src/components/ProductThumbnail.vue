99e2a351fc1565159bfda781e4b9692161988c46
Casper Pijnenburg <casperpijnenburg@gmail.com>
2018-11-21 10:37:37 +0100

    Ok

--- src/components/ProductThumbnail.vue
+++ src/components/ProductThumbnail.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-progress-circular :width="6" :size="80" :value="value" color="blue">
+  <v-progress-circular :width="7" :size="70" :style="{left: 7 + index * 33 + '%', top: '20px', position: 'absolute'}" :value="value" color="blue">
     <img :src="product.thumbnail" />
   </v-progress-circular>
 </template>
@@ -22,6 +22,10 @@ export default {
     },
     cancel: {
       type: Boolean,
+      required: true
+    },
+    index: {
+      type: Number,
       required: true
     }
   },
@@ -53,17 +57,14 @@ export default {
 </script>
 
 <style scoped>
-.product-thumbnail {
-  width: 33%;
-  height: 100%;
-  padding-top: 2vh;
-  text-align: center;
-  float: left;
-}
-
 img {
+  margin-top: 5px;
   height: 10vh;
   border-radius: 50%;
   border-radius: 50%;
+  -webkit-box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
+    0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+  box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
+    0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12);
 }
 </style>
